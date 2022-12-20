c7fc506ee4b9b20f103a85ea7d401988f6fc8634
chenzhiwen <191128770@qq.com>
2022-12-20 18:08:01 +0800

    :pencil: 添加md

--- config/index.js
+++ config/index.js
@@ -1,25 +1,26 @@
 const path = require('path')
 
-const REMOTE_PATH = `/usr/local/nginx/html/test_empty`
+const REMOTE_PATH = `远程服务器路径`
+// 本地资源文件
 const LOCAL_PATH = path.resolve(__dirname, '../dist')
 
 const config_aliyun = {
-  host: 'igrcphdvrh-public.bastionhost.aliyuncs.com',
-  port: '60022',
-  username: 'lmh',
-  password: '8uCJLgUR3Nz6Jwsu'
+  host: '堡垒机host',
+  port: '堡垒机端口号',
+  username: '堡垒机用户名',
+  password: '堡垒机密码'
 }
 
 const config_aliyun_server = {
-  host: '192.168.1.144',
+  host: '远程服务器host',
   port: '22'
 }
 
 const config_local_server = {
   host: '127.0.0.1',
-  port: '10144',
-  username: 'root',
-  password: ''
+  port: '本地端口',
+  username: '用户名',
+  password: '密码'
 }
 
 const config = {
